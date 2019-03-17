bbfb2949e3be0fb81bb2e2fca9ef5147495ad40c
KeerthiMettu <keertthi98@gmail.com>
2019-03-17 11:55:33 -0600

    intruder height identification complete

--- Project1.ino
+++ Project1.ino
@@ -2,8 +2,9 @@
 int trigger=2;
 int echo=3;
 int intruderBuzzer=12;
-const unsigned long duration=0;
+unsigned long duration;
 int distance=0;
+int entranceHeight=180; //180 cms
 
 
 const unsigned int BAUD_RATE=9600;
@@ -12,7 +13,7 @@ void setup() {
   // put your setup code here, to run once:
 pinMode(trigger, OUTPUT);
 pinMode(echo, INPUT);
-pinMode(intruderBuzzer, INPUT);
+pinMode(intruderBuzzer, OUTPUT);
 
 Serial.begin(BAUD_RATE);
 }
@@ -21,21 +22,26 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   digitalWrite(trigger,HIGH);
-  delay(1000);
+  delayMicroseconds(1000);
   digitalWrite(trigger, LOW);
 
   duration=pulseIn(echo,HIGH);
+  if(duration==0)
+  {
+    Serial.println("Warning from Ultrasonic sensor");
+  }
   distance=(duration/2)/29.1;
   Serial.print("cm = ");
-  Serial.print(distance);
-  if (Distance<50)
+  Serial.println(distance);
+  if (distance<140)
   {
    digitalWrite(intruderBuzzer,HIGH);
-   delay(1000);
+   Serial.print(entranceHeight-distance);
+   Serial.println("- this is intruder height");
+   delay(10);
   }
  else 
  digitalWrite(intruderBuzzer,LOW);
-
- delay(200);
+ delay(20);
 
 }
